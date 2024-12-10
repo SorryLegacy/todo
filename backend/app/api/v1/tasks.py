0d33d5d316c88a61822148e9f754ef727da886be
Alex Remnev <alexremnev2@gmail.com>
2024-12-10 14:50:29 +0300

    style fix

--- backend/app/api/v1/tasks.py
+++ backend/app/api/v1/tasks.py
@@ -1,38 +1,44 @@
 from typing import Annotated, Optional
-from fastapi import APIRouter, Depends, Query
-from database import Task, PatchTask, CreateTask, Notification, NotificationList
-from services.task import TaskService
-from depends import get_task_service
 from uuid import UUID
 
-router = APIRouter(tags=['task',])
+from database import CreateTask, NotificationList, PatchTask, Task
+from depends import get_task_service
+from fastapi import APIRouter, Depends, Query
+from services.task import TaskService
 
-taskService = Annotated['TaskService', Depends(get_task_service)]
+router = APIRouter(
+    tags=[
+        "task",
+    ]
+)
+
+taskService = Annotated["TaskService", Depends(get_task_service)]
 
 
-@router.get('/tasks', response_model=list[Task])
+@router.get("/tasks", response_model=list[Task])
 async def get_task(task_service: taskService, search: Optional[str] = Query(None)):
     return await task_service.list_all(search)
 
 
-@router.post('/tasks', response_model=Task)
+@router.post("/tasks", response_model=Task)
 async def create_task(new_task: CreateTask, task_service: taskService):
     task = await task_service.create(new_task)
-    return task 
+    return task
 
 
-@router.patch('/tasks/{task_id}', response_model=Task)
+@router.patch("/tasks/{task_id}", response_model=Task)
 async def update_taks(task_id: str, data: PatchTask, task_service: taskService):
     task = await task_service.patch_update(task_id=task_id, patch_task=data)
     return task
 
-@router.delete('/tasks/{task_id}')
+
+@router.delete("/tasks/{task_id}")
 async def delete_task(task_id: UUID, task_service: taskService):
-    print('HERE')
+    print("HERE")
     await task_service.delete_taks(task_id)
     return {"status": "Ok"}
 
 
-@router.get('/tasks/notifications', response_model=NotificationList)
+@router.get("/tasks/notifications", response_model=NotificationList)
 async def get_notifications(task_service: taskService):
     return await task_service.get_upcoming_deadlines_for_user()
--- backend/app/database/task.py
+++ backend/app/database/task.py
@@ -1,22 +1,24 @@
-from uuid import UUID
+from datetime import datetime
 from enum import Enum
 from typing import Optional
-from datetime import datetime, timedelta
-from sqlmodel import SQLModel, Field
-from .base import BaseModel
+from uuid import UUID
+
 from pydantic import ConfigDict
+from sqlmodel import Field, SQLModel
+
+from .base import BaseModel
 
 
 class Priority(Enum):
-    LOW = 'low'
-    MEDIUM = 'medium'
-    HIGH = 'high'
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
 
 
 class Status(Enum):
-    NEW = 'new'
-    IN_PROGRESS = 'in_progress'
-    COMPLETED = 'completed'
+    NEW = "new"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
 
 
 class BaseTask(SQLModel):
@@ -25,15 +27,16 @@ class BaseTask(SQLModel):
     description: Optional[str] = None
     priority: Priority
 
+
 class Task(BaseTask, BaseModel, table=True):
     status: Status
-    user_id: UUID = Field(foreign_key='users_users.id')
+    user_id: UUID = Field(foreign_key="users_users.id")
     last_notification: Optional[datetime] = None
 
 
 class CreateTask(BaseTask):
     status: Status = Field(default=Status.NEW, nullable=True)
-    model_config = ConfigDict(extra='allow')
+    model_config = ConfigDict(extra="allow")
 
 
 class PatchTask(SQLModel):
@@ -42,6 +45,3 @@ class PatchTask(SQLModel):
     deadline: Optional[datetime] = None
     priority: Optional[Priority] = None
     status: Optional[Status] = None
-
-
-from itertools import cycle
